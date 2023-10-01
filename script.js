985f23e1bef353a36dd29b2475a6e51d0e33fdc0
albossmohammad83 <albossmohammad83@gmail.com>
2023-10-01 00:35:55 -0400

    first c

---
+++ script.js
@@ -0,0 +1,100 @@
+const socket = new WebSocket('ws://localhost:8080');
+const chatOutput = document.getElementById('chat-output');
+
+
+document.getElementById('message-input').focus();
+function displaySentMessage(message) {
+  const messageElement = document.createElement('div');
+  messageElement.className = 'sent-message';
+  messageElement.innerHTML = message;
+  chatOutput.appendChild(messageElement);
+  scrollToBottom();
+}
+
+function displayReceivedMessage(message) {
+  const messageElement = document.createElement('div');
+  messageElement.className = 'received-message';
+  messageElement.innerHTML = message;
+  chatOutput.appendChild(messageElement);
+  scrollToBottom();
+  playNotificationSound();
+}
+
+socket.addEventListener('message', event => {
+  let messageData;
+  let messageTimestamp = getTimestamp();
+  // Check if the data is a JSON string
+  let isJSON = true;
+  try {
+    messageData = JSON.parse(event.data);
+  } catch (error) {
+    isJSON = false;
+  }
+
+  // Handle messages in JSON format
+  if (isJSON) {
+    displayReceivedMessage(`<p><strong>${messageData.nickname}:</strong> ${messageData.message}<span>${messageTimestamp}</span></p>`);
+  } else {
+    // If it's not JSON, handle it as a simple text message
+    const message = event.data;
+    displayReceivedMessage(`<p>${message}<span>${messageTimestamp}</span></p>`);
+  }
+});
+
+function sendMessage() {
+  const messageInput = document.getElementById('message-input');
+  const nickName = getNickname();
+  const timeStamp = getTimestamp();
+  const message = messageInput.value;
+  if (message.trim() !== '') {
+    const data = {
+      message: message,
+      nickname: nickName,
+    };
+    displaySentMessage(`<p><strong>${nickName}:</strong> ${message}<span>${timeStamp}</span></p>`);
+    socket.send(JSON.stringify(data));
+  }
+  messageInput.value = '';
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    sendMessage();
+  }  
+}
+
+function getTimestamp() {
+  const now = new Date();
+  const options = { hour: 'numeric', minute: '2-digit', hour12: true };
+  return now.toLocaleTimeString('en-US', options);
+}
+
+function submitNickname() {
+  const nickname = document.getElementById('nickname-input').value;
+  if (nickname.trim() !== '') {
+    document.getElementById('dialog-overlay').style.display = 'none'; 
+    document.getElementById('phone-frame').classList.remove('blurred'); 
+    // Save the nickname for future messages
+    localStorage.setItem('nickname', nickname);
+  }
+}
+
+function getNickname() {
+  return localStorage.getItem('nickname');
+}
+
+function scrollToBottom() {
+  chatOutput.scrollTop = chatOutput.scrollHeight;
+}
+
+function playNotificationSound() {
+  const notificationSound = new Audio('notification-sound.mp3');
+  // Play the sound when the user interacts with the document (e.g., clicking a button)
+  document.addEventListener('click', () => {
+    notificationSound.play()
+      .catch(error => {
+        console.error('Audio playback failed:', error);
+      });
+  });
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+  ws.send('Welcome Mohammad`s server!');
+
+  ws.on('message', message => {
+    
+    let parsedMessage;
+    try {
+      parsedMessage = JSON.parse(message);      
+      // Broadcast the message to all clients
+      wss.clients.forEach(client => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(parsedMessage));
+          console.log(JSON.stringify(parsedMessage));
+        }
+      });
+    } catch (error) {
+      console.error('Error parsing message as JSON:', error);
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket closed');
+  });
+});
